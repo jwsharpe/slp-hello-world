d36468fa69d7551ff4db516d8f930062ca39561a
James Sharpe <jameswrightsharpe@gmail.com>
2020-07-13 19:20:35 -0400

    added comments

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 
 const { SlippiGame, characters } = require('slp-parser-js');
+
+// node's filesystem API
 const fs = require("fs");
+
+// games directory to loop over.
 const gamesDir = "./Pound-2019/vgbootcamp";
 
 const files = fs.readdirSync(gamesDir);
 
+// convert files to slippi game objects
 const games = files.map((e) => new SlippiGame(gamesDir + "/" + e));
 
+//standard "memo"-ization to count character usage. 
 const count = {};
 games.forEach((game) => {
   game.getSettings().players.forEach((player) => {
