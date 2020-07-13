ebf7e6578f8ebba8aa976494f66b02f4ea74a9bc
James Sharpe <jameswrightsharpe@gmail.com>
2020-07-13 19:17:34 -0400

    removed dictionary in favor of the imported dictionary

--- app.js
+++ app.js
@@ -1,35 +1,8 @@
-const { SlippiGame } = require("slp-parser-js");
+
+const { SlippiGame, characters } = require('slp-parser-js');
 const fs = require("fs");
 const gamesDir = "./Pound-2019/vgbootcamp";
 
-const characterDictionary = [
-  "Captain Falcon",
-  "Donkey Kong",
-  "Fox",
-  "Mr. Game & Watch",
-  "Kirby",
-  "Bowser",
-  "Link",
-  "Luigi",
-  "Mario",
-  "Marth",
-  "Mewtwo",
-  "Ness",
-  "Peach",
-  "Pikachu",
-  "Ice Climbers",
-  "Jigglypuff",
-  "Samus",
-  "Yoshi",
-  "Zelda",
-  "Shiek",
-  "Falco",
-  "Young Link",
-  "Dr. Mario",
-  "Roy",
-  "Pichu",
-  "Ganondorf",
-];
 const files = fs.readdirSync(gamesDir);
 
 const games = files.map((e) => new SlippiGame(gamesDir + "/" + e));
@@ -37,7 +10,7 @@ const games = files.map((e) => new SlippiGame(gamesDir + "/" + e));
 const count = {};
 games.forEach((game) => {
   game.getSettings().players.forEach((player) => {
-    const character = characterDictionary[player.characterId];
+    const character = characters.getCharacterName(player.characterId);
     if (count[character] && player.type === 0) {
       count[character] += 1;
     } else {
